46cbe3d9e99b2b896565fd71fa81fbafc2bf9531
Marto <Cookieoxen@gmail.com>
2020-12-26 12:41:37 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MartosApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
